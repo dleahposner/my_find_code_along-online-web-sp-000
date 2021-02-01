10ca560a3f8f7db5f10c6cc42aa2aecbe4be9e0f
Devorah Posner <dleahposner@gmail.com>
2021-02-01 23:04:55 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -8,17 +8,3 @@ def my_find(collection)
 end
 end
 
-# def my_all?(collection)
-#   i = 0 
-#   block_return_values = []
-#   while i < collection.length 
-#   block_return_values << yield(collection[i])
-#   i = i + 1 
-# end
-# if block_return_values.include?(false)
-#     false
-#   else
-#     true
-#   end
-
-# end
