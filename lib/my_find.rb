70ae83ad070f8ba262803e63e3eeedf69ead6d0a
Devorah Posner <dleahposner@gmail.com>
2021-02-01 23:04:36 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,24 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0 
+  while i < collection.length 
+    return collection[i] if yield(collection[i])
+  i = i + 1 
 end
+end
+
+# def my_all?(collection)
+#   i = 0 
+#   block_return_values = []
+#   while i < collection.length 
+#   block_return_values << yield(collection[i])
+#   i = i + 1 
+# end
+# if block_return_values.include?(false)
+#     false
+#   else
+#     true
+#   end
+
+# end
